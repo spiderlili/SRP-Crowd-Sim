639b6d9d56bfb02bff1782829568049b3f54bad2
Jing Chun Tan <jtan1993@gmail.com>
2019-06-02 12:24:07 +0100

    init

---
+++ SRP-Crowd-Sim/Assets/Scripts/CustomPipelineTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Pipeline Test")]
+public class CustomPipelineTest : RenderPipelineAsset {
+    protected override RenderPipeline CreatePipeline()
+    {
+        return null;
+    }
+
+}
---
+++ SRP-Crowd-Sim/Assets/Scripts/MyPipelineInstance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MyPipelineInstance : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext renderContext, Camera[] cameras)
+    {
+        //base.Render(renderContext, cameras);
+        renderContext.DrawSkybox(cameras[0]);
+        renderContext.Submit();
+    }
+
+    /*
+    protected override RenderPipeline InternalCreatePipeline()
+    {
+        return new MyPipeline();
+    }*/
+
+}
